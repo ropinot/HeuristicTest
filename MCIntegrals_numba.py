53981e3815b1de80ad78e1121d708ab99483f7ce
Roberto <robero>
2015-07-24 23:20:18 +0200

    Using Numba

--- MCIntegrals_numba.py
+++ MCIntegrals_numba.py
@@ -1,6 +1,8 @@
 from scipy.stats import truncnorm
 from numpy import ndarray, min
 from numba import jit
+from math import trunc
+
 
 parameters = {'Q1': 38.0,
               'Q2': 67.0,
@@ -51,12 +53,20 @@ parameters = {'Q1': 38.0,
               'alpha': 0.5,
               'A': 130.,
               'N': 200000,
-              'scaling': False,
+              'scaling': True,
               }
 
 @jit(nopython=True)
 def MIN(a, b):
-        return a if a <= b else b
+    return a if a <= b else b
+
+@jit(nopython=True)
+def MAX(a, b):
+    return a if a >= b else b
+
+@jit(nopython=True)
+def ABS(a):
+    return a if a > 0.0 else -a
 
 
 def f3TruncNormRVSnp(parameters):
@@ -71,15 +81,80 @@ def f3TruncNormRVSnp(parameters):
     rv2 = truncnorm(a2, b2, loc=parameters['mu2'], scale=parameters['sigma2']).rvs(N)
     rv3 = truncnorm(a3, b3, loc=parameters['mu3'], scale=parameters['sigma3']).rvs(N)
 
+    if parameters['scaling']:
+        #scale the values of Qs in the allowed range such that sum(Q_i) = A
+        r = abs(parameters['Q1']) + abs(parameters['Q2']) + abs(parameters['Q3'])
+        if r == 0.0:
+            r = 1.
+        # rounding the values, the sum could exceed A
+        Q1 = trunc(ABS(parameters['Q1']) * parameters['A'] / r)
+        Q2 = trunc(ABS(parameters['Q2']) * parameters['A'] / r)
+        Q3 = parameters['A'] - Q1 - Q2
+    else:
+        # print "scaling = False"
+        Q1 = parameters['Q1']
+        Q2 = parameters['Q2']
+        Q3 = parameters['Q3']
+
+    return _f3(rv1, rv2, rv3, Q1, Q2, Q3, target, N)
+
+
+@jit(nopython=True)
+def _f3(rv1, rv2, rv3, Q1, Q2, Q3, target, N):
     hit = 0
     not_hit = 0
     for i in range(N):
-        if MIN(rv1[i], parameters['Q1']) + MIN(rv2[i], parameters['Q2']) + MIN(rv3[i], parameters['Q3']) >= target:
+        if MIN(rv1[i], Q1) + MIN(rv2[i], Q2) + MIN(rv3[i], Q3) >= target:
             hit += 1.
         else:
             not_hit += 1.
 
     return float(hit)/N
+
+
+# def f3TruncNormRVSnp(parameters):
+#     N = parameters['N']
+#     target = parameters['target']
+#     rv1, rv2, rv3 = ndarray(shape = (N,), dtype=float), ndarray(shape = (N,), dtype=float), ndarray(shape = (N,), dtype=float)
+#
+#     a1, b1 = (parameters['min_intrv1'] - parameters['mu1']) / parameters['sigma1'], (parameters['max_intrv1'] - parameters['mu1']) / parameters['sigma1']
+#     a2, b2 = (parameters['min_intrv2'] - parameters['mu2']) / parameters['sigma2'], (parameters['max_intrv2'] - parameters['mu2']) / parameters['sigma2']
+#     a3, b3 = (parameters['min_intrv3'] - parameters['mu3']) / parameters['sigma3'], (parameters['max_intrv3'] - parameters['mu3']) / parameters['sigma3']
+#     rv1 = truncnorm(a1, b1, loc=parameters['mu1'], scale=parameters['sigma1']).rvs(N)
+#     rv2 = truncnorm(a2, b2, loc=parameters['mu2'], scale=parameters['sigma2']).rvs(N)
+#     rv3 = truncnorm(a3, b3, loc=parameters['mu3'], scale=parameters['sigma3']).rvs(N)
+#
+#     if parameters['scaling']:
+#         #scale the values of Qs in the allowed range such that sum(Q_i) = A
+#         r = abs(parameters['Q1']) + abs(parameters['Q2']) + abs(parameters['Q3'])
+#         if r == 0.0:
+#             r = 1.
+#
+#         # rounding the values, the sum could exceed A
+#         Q1 = trunc(ABS(parameters['Q1']) * parameters['A'] / r)
+#         Q2 = trunc(ABS(parameters['Q2']) * parameters['A'] / r)
+#         Q3 = parameters['A'] - Q1 - Q2
+#     else:
+#         # print "scaling = False"
+#         Q1 = parameters['Q1']
+#         Q2 = parameters['Q2']
+#         Q3 = parameters['Q3']
+#
+#     hit = 0
+#     not_hit = 0
+#     print "NM Start"
+#     for t in xrange(1, 4):
+#         print "Q{}: {}".format(t, parameters['Q{}'.format(t)])
+#
+#     for i in range(N):
+#         if MIN(rv1[i], Q1) + MIN(rv2[i], Q2) + MIN(rv3[i], Q3) >= target:
+#             hit += 1.
+#         else:
+#             not_hit += 1.
+#
+#     print "NM End - P: {}".format(float(hit)/N)
+#
+#     return float(hit)/N
 
 
 if __name__ == "__main__":
