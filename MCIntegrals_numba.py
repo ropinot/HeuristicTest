38cebc61e1e75cf8d73834ae39faf5d5054b8b02
Roberto <robero>
2015-07-26 00:41:38 +0200

    Lot of changes, still unsure about the results

--- MCIntegrals_numba.py
+++ MCIntegrals_numba.py
@@ -2,7 +2,7 @@ from scipy.stats import truncnorm
 from numpy import ndarray, min
 from numba import jit
 from math import trunc
-
+import multiprocessing as mp
 
 parameters = {'Q1': 38.0,
               'Q2': 67.0,
@@ -74,6 +74,14 @@ def f3TruncNormRVSnp(parameters):
     target = parameters['target']
     rv1, rv2, rv3 = ndarray(shape = (N,), dtype=float), ndarray(shape = (N,), dtype=float), ndarray(shape = (N,), dtype=float)
 
+    # if parameters['ncpu']:
+    #     ncpu = parameters['ncpu']
+    # else:
+    #     ncpu = mp.cpu_count()
+    #
+    # pool = mp.Pool(ncpu)
+    # workers = []
+
     a1, b1 = (parameters['min_intrv1'] - parameters['mu1']) / parameters['sigma1'], (parameters['max_intrv1'] - parameters['mu1']) / parameters['sigma1']
     a2, b2 = (parameters['min_intrv2'] - parameters['mu2']) / parameters['sigma2'], (parameters['max_intrv2'] - parameters['mu2']) / parameters['sigma2']
     a3, b3 = (parameters['min_intrv3'] - parameters['mu3']) / parameters['sigma3'], (parameters['max_intrv3'] - parameters['mu3']) / parameters['sigma3']
@@ -97,22 +105,37 @@ def f3TruncNormRVSnp(parameters):
         Q2 = parameters['Q2']
         Q3 = parameters['Q3']
 
+    return _f3(rv1, rv2, rv3, Q1, Q2, Q3, target)
+
+    # Version with multiprocessing
+    # _N = int(N/4.)
+    # workers.append(pool.apply_async(_f3, args=(rv1[0:_N], rv2[0:_N], rv3[0:_N],Q1, Q2, Q3, target)))
+    # workers.append(pool.apply_async(_f3, args=(rv1[_N:2*_N], rv2[_N:2*_N], rv3[_N:2*_N], Q1, Q2, Q3, target)))
+    # workers.append(pool.apply_async(_f3, args=(rv1[2*_N:3*_N], rv2[2*_N:3*_N], rv3[2*_N:3*_N], Q1, Q2, Q3, target)))
+    # workers.append(pool.apply_async(_f3, args=(rv1[3*_N:N], rv2[3*_N:N], rv3[3*_N:N], Q1, Q2, Q3, target)))
+    #
+    # hit = 0.0
+    # for w in workers:
+    #     hit += w.get()
+    # pool.close()
+    #
+    # return float(hit/N)
     # call the compiled function that perform the calculation loop
-    return _f3(rv1, rv2, rv3, Q1, Q2, Q3, target, N)
 
 
 @jit(nopython=True)
-def _f3(rv1, rv2, rv3, Q1, Q2, Q3, target, N):
-    hit = 0
-    not_hit = 0
-    for i in range(N):
+def _f3(rv1, rv2, rv3, Q1, Q2, Q3, target):
+    hit = 0.
+    N = len(rv1)
+    # not_hit = 0
+
+    for i in xrange(N):
         if MIN(rv1[i], Q1) + MIN(rv2[i], Q2) + MIN(rv3[i], Q3) >= target:
             hit += 1.
-        else:
-            not_hit += 1.
+        # else:
+        #     not_hit += 1.
 
-    return float(hit)/N
-
+    return hit/N
 
 # def f3TruncNormRVSnp(parameters):
     # This version does not use the numba function _f3
--- test_heuristic_on_large_problem.py
+++ test_heuristic_on_large_problem.py
@@ -8,7 +8,6 @@ from random import random, randint
 import time
 import logging
 
-
 logging.basicConfig(level=logging.DEBUG)
 
 parameters['scaling'] = True
@@ -17,63 +16,91 @@ parameters['N'] = 200000
 parameters['xtol'] = 1.
 parameters['ftol'] = 0.1
 parameters['numrun'] = 10
+parameters['ncpu'] = 4
 
-df = pd.read_excel('Data_alloc_change_on_target_change_comp_NM_large_problem_test.xlsx')
-df_results = pd.read_excel('Results_alloc_change_on_target_change_comp_NM_large_problem.xlsx')
+if __name__ == '__main__':
 
-for index, row in df.iterrows():
-    # get from excel file
-    logging.debug('Start index {} (dataset {})'.format(index, row['DATASET']))
+    df = pd.read_excel('Data_alloc_change_on_target_change_comp_NM_large_problem_test.xlsx')
+    df_results = pd.read_excel('Results_alloc_change_on_target_change_comp_NM_large_problem.xlsx')
 
-    parameters['mu1'], parameters['mu2'], parameters['mu3'] = row['MU1'],row['MU2'],row['MU3']
-    parameters['sigma1'], parameters['sigma2'], parameters['sigma3'] = row['S1'],row['S2'],row['S3']
-    parameters['A'] = row['AVAIL']
-    parameters['target'] = row['TARGET']
-    parameters['max_intrv1'], parameters['max_intrv2'], parameters['max_intrv3'] = row['MAX_INTRV'], row['MAX_INTRV'], row['MAX_INTRV']
-    parameters['dataset'] = row['DATASET']
+    df_results_columns = [u'DATASET', u'N', u'AVAIL', u'TARGET', u'MU1', u'S1', u'MU2', u'S2', u'MU3', u'S3', u'MAX_INTRV', u'NM_PROB', u'NM_TIME', u'NUM_ITER', u'FUN_EVAL', u'Q1', u'Q2', u'Q3', u'ALLOC_HEU', u'HEURISTIC_VALUE', u'HEURISTIC_TIME']
 
-    logging.debug('Start greedy heuristics')
-    start_time = time.time()
-    result_heu = greedy_allocation3(parameters)
-    end_time = time.time()
-    heu_time = end_time - start_time
-    logging.debug('Greedy heuristics ended')
+    for index, row in df.iterrows():
+        # get from excel file
+        logging.debug('Start index {} (dataset {})'.format(index, row['DATASET']))
 
-    numrun = parameters['numrun']
-    for t in xrange(numrun):
+        parameters['mu1'], parameters['mu2'], parameters['mu3'] = row['MU1'],row['MU2'],row['MU3']
+        parameters['sigma1'], parameters['sigma2'], parameters['sigma3'] = row['S1'],row['S2'],row['S3']
+        parameters['A'] = row['AVAIL']
+        parameters['target'] = row['TARGET']
+        parameters['max_intrv1'], parameters['max_intrv2'], parameters['max_intrv3'] = row['MAX_INTRV'], row['MAX_INTRV'], row['MAX_INTRV']
+        parameters['dataset'] = row['DATASET']
 
-        parameters['Q1'], parameters['Q2'], parameters['Q3'] = randint(100, 600), randint(100, 600), randint(100, 600)
-        logging.debug('Start NM run {}'.format(t+1))
+        logging.debug('Start greedy heuristics')
         start_time = time.time()
-        result_nm = nm(parameters)
+        result_heu = greedy_allocation3(parameters)
         end_time = time.time()
-        nm_time = end_time - start_time
-        logging.debug('NM run {} ended'.format(t+1))
+        heu_time = end_time - start_time
+        logging.debug('Greedy heuristics ended')
 
-        logging.debug('Write on DF')
-        df_results.ix[index, 'DATASET'] = row['DATASET']
-        df_results.ix[index, 'N'] = 3
-        df_results.ix[index, 'AVAIL'] = parameters['A']
-        df_results.ix[index, 'TARGET'] = parameters['target']
-        df_results.ix[index, 'MU1'] = parameters['mu1']
-        df_results.ix[index, 'MU2'] = parameters['mu2']
-        df_results.ix[index, 'MU3'] = parameters['mu3']
-        df_results.ix[index, 'S1'] = parameters['sigma1']
-        df_results.ix[index, 'S2'] = parameters['sigma2']
-        df_results.ix[index, 'S3'] = parameters['sigma3']
-        df_results.ix[index, 'MAX_INTRV'] = row['MAX_INTRV']
-        df_results.ix[index, 'NM_PROB'] = result_nm.fun
-        df_results.ix[index, 'NM_TIME'] = nm_time
-        df_results.ix[index, 'NUM_ITER'] = result_nm.nit
-        df_results.ix[index, 'FUN_EVAL'] = result_nm.nfev
-        df_results.ix[index, 'Q1'] = result_nm.x[0]
-        df_results.ix[index, 'Q2'] = result_nm.x[1]
-        df_results.ix[index, 'Q3'] = result_nm.x[2]
-        df_results.ix[index, 'ALLOC_HEU'] = str([result_heu['Q1'], result_heu['Q2'], result_heu['Q3']])
-        df_results.ix[index, 'HEURISTIC_VALUE'] = result_heu['PROB']
-        df_results.ix[index, 'HEURISTIC_TIME'] = heu_time
+        numrun = parameters['numrun']
+        for t in xrange(numrun):
 
-logging.debug('Write on disk')
-df_results.to_excel('Results.xlsx')
+            parameters['Q1'], parameters['Q2'], parameters['Q3'] = randint(100, 600), randint(100, 600), randint(100, 600)
+            logging.debug('Start NM run {}'.format(t+1))
+            start_time = time.time()
+            result_nm = nm(parameters)
+            end_time = time.time()
+            nm_time = end_time - start_time
+            logging.debug('NM run {} ended'.format(t+1))
 
-logging.debug('Program complete')
+            logging.debug('Create results DF')
+            # df_results.ix[index, 'DATASET'] = row['DATASET']
+            # df_results.ix[index, 'N'] = 3
+            # df_results.ix[index, 'AVAIL'] = parameters['A']
+            # df_results.ix[index, 'TARGET'] = parameters['target']
+            # df_results.ix[index, 'MU1'] = parameters['mu1']
+            # df_results.ix[index, 'MU2'] = parameters['mu2']
+            # df_results.ix[index, 'MU3'] = parameters['mu3']
+            # df_results.ix[index, 'S1'] = parameters['sigma1']
+            # df_results.ix[index, 'S2'] = parameters['sigma2']
+            # df_results.ix[index, 'S3'] = parameters['sigma3']
+            # df_results.ix[index, 'MAX_INTRV'] = row['MAX_INTRV']
+            # df_results.ix[index, 'NM_PROB'] = result_nm.fun
+            # df_results.ix[index, 'NM_TIME'] = nm_time
+            # df_results.ix[index, 'NUM_ITER'] = result_nm.nit
+            # df_results.ix[index, 'FUN_EVAL'] = result_nm.nfev
+            # df_results.ix[index, 'Q1'] = result_nm.x[0]
+            # df_results.ix[index, 'Q2'] = result_nm.x[1]
+            # df_results.ix[index, 'Q3'] = result_nm.x[2]
+            # df_results.ix[index, 'ALLOC_HEU'] = str([result_heu['Q1'], result_heu['Q2'], result_heu['Q3']])
+            # df_results.ix[index, 'HEURISTIC_VALUE'] = result_heu['PROB']
+            # df_results.ix[index, 'HEURISTIC_TIME'] = heu_time
+            df_run_result = pd.DataFrame([[row['DATASET'],
+                                          3,
+                                          parameters['A'],
+                                          parameters['target'],
+                                          parameters['mu1'],
+                                          parameters['mu2'],
+                                          parameters['mu3'],
+                                          parameters['sigma1'],
+                                          parameters['sigma2'],
+                                          parameters['sigma3'],
+                                          row['MAX_INTRV'],
+                                          result_nm.fun,
+                                          nm_time,
+                                          result_nm.nit,
+                                          result_nm.nfev,
+                                          result_nm.x[0],
+                                          result_nm.x[1],
+                                          result_nm.x[2],
+                                          str([result_heu['Q1'], result_heu['Q2'], result_heu['Q3']]),
+                                          result_heu['PROB'],
+                                          heu_time]], columns=df_results_columns)
+
+            df_results=df_results.append(df_run_result, ignore_index=True)
+
+    logging.debug('Write on disk')
+    df_results.to_excel('Results.xlsx')
+
+    logging.debug('Program complete')
